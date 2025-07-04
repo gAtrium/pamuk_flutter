1df9555e3487f70599f134992a9ac26632e7db1f
gAtrium <pdimetil@gmail.com>
2025-07-04 15:47:19 +0300

    update workflow

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'services/pamuk_provider.dart';
 import 'services/locale_provider.dart';
